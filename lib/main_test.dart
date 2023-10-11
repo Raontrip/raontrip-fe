e8006e1d3cd3be76008ee958224e0e50cbdb8373
김수연 <1108suyeon@gmail.com>
2023-10-11 16:41:35 +0900

    Fix minor error

--- lib/main_test.dart
+++ lib/main_test.dart
@@ -93,7 +93,7 @@ class RankProviders {
 
   // Future getEngRank() async {
   getEngRank(int lang) async {
-    Uri u = Uri.parse(mainUrl + "lang=$lang&size=10  cat");
+    Uri u = Uri.parse(mainUrl + "lang=$lang&size=10");
     final response = await http.get(u);
 
     if (jsonDecode(response.body)['code'] == 20002) {
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/johyunchol/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/johyunchol/Documents/workspace/kkensu/kakao_map_plugin/example"
+export "FLUTTER_ROOT=C:\AndroidStudio\3.0.6\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\src\flutterwork\ex2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
